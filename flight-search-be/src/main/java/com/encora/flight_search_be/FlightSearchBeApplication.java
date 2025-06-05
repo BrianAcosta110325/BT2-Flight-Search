a57d493ba3f09f8f9f79f31122979266308fc141
Brian A <brian.acosta@encora.com>
2025-06-05 13:24:36 -0600

    first commit

---
+++ flight-search-be/src/main/java/com/encora/flight_search_be/FlightSearchBeApplication.java
@@ -0,0 +1,13 @@
+package com.encora.flight_search_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSearchBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSearchBeApplication.class, args);
+	}
+
+}
---
+++ flight-search-be/src/test/java/com/encora/flight_search_be/FlightSearchBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encora.flight_search_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSearchBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
