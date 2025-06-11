b712b21ec49607176481b33a234c7746e9e22351
Brian Acosta <brian.acosta@encora.com>
2025-06-11 17:43:01 -0600

    add flight_search controller

--- flight-search-be/src/main/java/com/encora/flight_search_be/controller/FlightSearchController.java
+++ flight-search-be/src/main/java/com/encora/flight_search_be/controller/FlightSearchController.java
@@ -1,18 +1,37 @@
 package com.encora.flight_search_be.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.encora.flight_search_be.service.FlightSeacrchService;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:3000")
 public class FlightSearchController {
+
+    @Autowired
+    private FlightService flightService;
     
-    @GetMapping("/flights")
-    public String getFlights() {
-        // This is a placeholder for the actual flight search logic
-        return "OH YEAH BABY 6";
+    @GetMapping("/search_flights")
+    public String getFlights(
+        @RequestParam(required = false) String departure_code,
+        @RequestParam(required = false) Integer arrival_code, 
+        @RequestParam(required = false) LocalDate departure_date,
+        @RequestParam(required = false) Number no_adults,
+        @RequestParam(required = false) String currency,
+        @RequestParam(required = false) Boolean nonStops
+    ) {
+        return this.flightService.getFlights(
+            departure_code, 
+            arrival_code, 
+            departure_date, 
+            no_adults, 
+            currency, 
+            nonStops
+        );
     }
 }
---
+++ flight-search-be/src/main/java/com/encora/flight_search_be/service/FlightSeacrchService.java
@@ -0,0 +1,5 @@
+package main.java.com.encora.flight_search_be.service;
+
+public class FlightSeacrchService {
+    
+}
