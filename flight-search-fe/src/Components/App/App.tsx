54734b4643ebc8b873e94d5a99abf35cee80ff39
Brian Acosta <brian.acosta@encora.com>
2025-06-18 19:03:41 -0600

    add app basic variables

--- flight-search-fe/src/Components/App/App.tsx
+++ flight-search-fe/src/Components/App/App.tsx
@@ -2,17 +2,26 @@ import { useState } from 'react';
 import { QueryParams } from '../../Interfaces/QueryParams';
 import Filter from '../Filter/Filter';
 import './App.css';
+import { Flight } from '../../Interfaces/Flight';
 
 function App() {
+  // Flights
+  const [flights, setFlights] = useState<Flight[]>([]);
+  // Pagination
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
-  // Filter
-  const [filterParams, setFilterParams] = useState<QueryParams>();
+  // FilterParams
+  const [filter, setFilter] = useState<QueryParams>({} as QueryParams);
+
+  // Loading state
+  const [loading, setLoading] = useState(true);
 
   return (
     <div className="App">
       <Filter
         onApplyFilter={(params) => {
-          setFilterParams((prev) => prev
+          setFilter((prev) => prev
             ? { ...prev, ...params }
             : { ...params } as QueryParams
           );
