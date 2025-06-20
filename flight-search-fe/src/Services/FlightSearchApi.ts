a3e861ce5fd740378a27e93c2d4dad1363efb6ea
Brian Acosta <brian.acosta@encora.com>
2025-06-19 21:39:32 -0600

    add api and flightSearch Api

---
+++ flight-search-fe/src/Services/FlightSearchApi.ts
@@ -0,0 +1,17 @@
+import { QueryParams } from "../Interfaces/QueryParams"
+import { Flight } from "../Interfaces/Flight"
+import { Api } from "./API"
+
+export const SearchFlightsService = {
+    getFlights: async (params?: QueryParams) => {
+        return Api.get(`searchFlights`, params)
+    },
+
+    getDetailedFlight: async (flight: Flight) => {
+        return Api.get(`searchFlightById/${flight.id}`)
+    },
+
+    getAirports: async (query: String) => {
+        return Api.get(`searchAirports/${query}`)
+     }
+ }
